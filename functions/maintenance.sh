f308ab1af7c012bd3c6bd880951b4dac9a0220d9
Kevin Ahrens <kevin@vastgoedflow.nl>
2024-06-20 08:39:07 +0200

    WIP

--- functions/maintenance.sh
+++ functions/maintenance.sh
@@ -15,16 +15,16 @@ function enable_maintenance_mode() {
 # Function to disable maintenance mode
 function disable_maintenance_mode() {
 
+  disable_hetrix_maintenance_mode
+  
+  slack_message $SLACK_CHANNEL "Stopping maintenance on the host \"$MACHINE_NAME\""
+
   if ! command -v zip &> /dev/null; then
       log_error $FUNCTION_NAME "Please install zip first (apt install zip)..."
       exit 1
   else
     zip -rj $APP_DIRECTORY"/logs.zip" $APP_DIRECTORY"/logs" >/dev/null 2>&1 </dev/null >> logs/zip.txt
   fi
-
-  disable_hetrix_maintenance_mode
-  
-  slack_message $SLACK_CHANNEL "Stopping maintenance on the host \"$MACHINE_NAME\""
 
   rm $APP_DIRECTORY"/.maintenance"
   rm -r $APP_DIRECTORY"/logs"
